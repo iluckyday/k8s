35db8988fe09640e7ac1be247981d923e36de8b4
iluckyday <luckyday1990@mail.com>
2020-02-15 19:58:13 +0000

    test

--- minikube/build-cache.sh
+++ minikube/build-cache.sh
@@ -5,4 +5,5 @@ minikube start --download-only=true --vm-driver=kvm2
 ver=$(basename `ls -d $HOME/.minikube/cache/v*`)
 cd $HOME
 rm -rf .minikube/cache/images/k8s.gcr.io
-tar -cJf /dev/shm/minikube-cache-$ver.tar.xz .minikube/bin/ .minikube/cache/
+tar -czf /dev/shm/minikube-cache-$ver.tar.gz .minikube/bin/ .minikube/cache/
+ls -lh /dev/shm/minikube-cache-$ver.tar.gz
--- minikube/ffsend.sh
+++ minikube/ffsend.sh
@@ -5,4 +5,4 @@ ffsend_ver="$(curl -skL https://api.github.com/repos/timvisee/ffsend/releases/la
 curl -skL -o /tmp/ffsend https://github.com/timvisee/ffsend/releases/download/"$ffsend_ver"/ffsend-"$ffsend_ver"-linux-x64-static
 chmod +x /tmp/ffsend
 
-/tmp/ffsend -Ifyq upload /dev/shm/minikube-cache-*.tar.xz
+/tmp/ffsend -Ifyq upload /dev/shm/minikube-cache-*
