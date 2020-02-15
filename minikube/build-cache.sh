dc4ef202da755598140e6ac22959c43f95eddb1e
iluckyday <luckyday1990@mail.com>
2020-02-15 19:22:04 +0000

    test

--- minikube/build-cache.sh
+++ minikube/build-cache.sh
@@ -4,4 +4,5 @@ set -e
 minikube start --download-only=true --vm-driver=kvm2
 ver=$(basename `ls -d $HOME/.minikube/cache/v*`)
 cd $HOME
+rm -rf .minikube/cache/images/k8s.gcr.io
 tar -cJf /dev/shm/minikube-cache-$ver.tar.xz .minikube/bin/ .minikube/cache/
