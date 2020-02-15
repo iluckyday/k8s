fd55e8af5bdbc4e4295d3ffa6e95468357a00cee
iluckyday <luckyday1990@mail.com>
2020-02-15 09:41:04 +0000

    test

--- minikube/build-cache.sh
+++ minikube/build-cache.sh
@@ -3,4 +3,5 @@ set -e
 
 minikube start --download-only=true --vm-driver=kvm2 --v=10 --alsologtostderr
 ver=$(basename `ls -d $HOME/.minikube/cache/v*`)
-tar -cJf /dev/shm/minikube-cache-$ver.tar.xz $HOME/.minikube/bin/ $HOME/.minikube/cache/
+cd $HOME
+tar -cJf /dev/shm/minikube-cache-$ver.tar.xz .minikube/bin/ .minikube/cache/
