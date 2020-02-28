47d8bdaaf06e25c5fac6a535715960aba2a59239
iluckyday <luckyday1990@mail.com>
2020-02-28 02:38:00 +0000

    test

--- boot2kube/build.sh
+++ boot2kube/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/buildroot/buildroot
-make -j2 ../boot2kube/build.config -C buildroot
+make -j2 BOOT2KUBE=../../boot2kube ../../boot2kube/build.config -C buildroot
 make -j2 -C buildroot
 cp buildroot/output/images/rootfs.iso9660 /tmp/boot2kube.iso
