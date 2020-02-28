2daf3a90311e2bb3d052d9948a85d79cc1f67ce6
iluckyday <luckyday1990@mail.com>
2020-02-28 04:23:46 +0000

    test

--- boot2kube/build.sh
+++ boot2kube/build.sh
@@ -2,7 +2,7 @@
 
 git clone --depth=1 https://github.com/buildroot/buildroot
 export BOOT2KUBE=$HOME/work/k8s/k8s/boot2kube KCONFIG_ALLCONFIG=$HOME/work/k8s/k8s/boot2kube/build.defconfig
-cd builroot
-make allnoconfig
-make -j2
+cd buildroot
+make -s -j"$(nproc)" allnoconfig
+make -s -j"$(nproc)"
 cp output/images/rootfs.iso9660 /tmp/boot2kube.iso
