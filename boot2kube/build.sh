83d08373f2f979e80eb9ef783421b460640e6926
iluckyday <luckyday1990@mail.com>
2020-03-02 13:34:18 +0000

    test

--- boot2kube/build.sh
+++ boot2kube/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 apt-get update
 apt-get install -y libelf-dev
@@ -6,7 +7,8 @@ apt-get install -y libelf-dev
 git clone --depth=1 https://github.com/buildroot/buildroot
 export KCONFIG_ALLCONFIG=$HOME/work/k8s/k8s/boot2kube/build.config
 cd buildroot
-make BOOT2KUBE_PATH=../boot2kube -s -j"$(nproc)" allnoconfig
+#make BOOT2KUBE_PATH=../boot2kube -s -j"$(nproc)" allnoconfig
+make -s -j"$(nproc)" allnoconfig
 make -s -j"$(nproc)"
 
 d=$(date "+%Y%m%d")
