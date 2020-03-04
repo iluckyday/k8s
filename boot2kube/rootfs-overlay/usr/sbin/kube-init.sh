2d271deebc1774343c4f3a820484f18f6a5638b8
iluckyday <luckyday1990@mail.com>
2020-03-04 00:24:23 +0000

    test

--- boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
+++ boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
@@ -14,9 +14,14 @@ ip route add default via $GW
 
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 
-until curl -skLo /tmp/run.sh http://server/boot2kube/run.sh
-do
-	sleep 2
+i=0
+while [ $i -lt 5 ]; do
+((i++))
+curl -skLo /tmp/run.sh http://server/boot2kube/run.sh
+if [ $? -eq 0 ]; then
+	break
+fi
 done
+unset i
 
-source /tmp/run.sh && rm -f /tmp/run.sh || exit
+[ -r /tmp/run.sh ] && source /tmp/run.sh && rm -f /tmp/run.sh
