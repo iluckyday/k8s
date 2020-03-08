28675ea12cc97359bfeb5610c4da70b0e396e36e
iluckyday <luckyday1990@mail.com>
2020-03-08 09:13:53 +0000

    test

--- boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
+++ boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-MAC=$(cat /sys/class/net/eth0/address)
-IP1=$((16#$(echo $MAC | awk -F: '{print $3}')))
-IP2=$((16#$(echo $MAC | awk -F: '{print $4}')))
-IP3=$((16#$(echo $MAC | awk -F: '{print $5}')))
-IP4=$((16#$(echo $MAC | awk -F: '{print $6}')))
-IP=$IP1.$IP2.$IP3.$IP4
-GW=$IP1.$IP2.$IP3.1
-
-ip link set eth0 up
-ip address add $IP/24 dev eth0
-ip route add default via $GW
-
-echo nameserver 1.1.1.1 > /etc/resolv.conf
+udhcpc -n -q -R -f -i ens10
 
 i=0
 while [ $i -lt 5 ]; do
@@ -22,7 +10,5 @@ if [ $? -eq 0 ]; then
 	break
 fi
 done
-unset i
 
 [ -r /tmp/run.sh ] && source /tmp/run.sh && rm -f /tmp/run.sh
-rm -f -- "$0"
