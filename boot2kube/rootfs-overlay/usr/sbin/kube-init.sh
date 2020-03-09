24e7dccec690067fe9425d63fc4c649dcba96567
iluckyday <luckyday1990@mail.com>
2020-03-09 12:36:29 +0000

    test

--- boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
+++ boot2kube/rootfs-overlay/usr/sbin/kube-init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+ii=0
+while [ $ii -lt 5 ]; do
+((ii++))
 dhcp_nic=$(ls -d /sys/class/net/en* | awk -F'/' '{print $5;exit}')
-test -n "$dhcp_nic" && udhcpc -n -q -f -i "$dhcp_nic" > /dev/null 2>&1 || exit
-
-i=0
-while [ $i -lt 5 ]; do
-((i++))
+udhcpc -n -q -f -i $dhcp_nic > /dev/null 2>&1 || continue
 curl -skLo /tmp/run.sh http://server/boot2kube/run.sh
 if [ $? -eq 0 ]; then
 	break
 fi
+sleep 1
 done
 
 [ -r /tmp/run.sh ] && source /tmp/run.sh && rm -f /tmp/run.sh || exit
