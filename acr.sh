17a3ab3e3331779c758ee297763a2bf51cf0c476
iluckyday <luckyday1990@mail.com>
2020-10-21 09:13:32 +0800

    test

--- acr.sh
+++ acr.sh
@@ -7,6 +7,7 @@ namespace=$4
 
 filename="$(basename $file)"
 filepath="$(dirname $file)"
+filesize="$(du -h $file | awk '{print $1}')"
 
 image="$filename:$tag"
 cd $filepath
@@ -14,3 +15,5 @@ tar -c $filename | docker import - registry.${region}.aliyuncs.com/${namespace}/
 docker login -u ${ACR_DOCKER_USERNAME} -p ${ACR_DOCKER_PASSWORD} registry.${region}.aliyuncs.com
 docker push registry.${region}.aliyuncs.com/${namespace}/${image}
 docker logout registry.${region}.aliyuncs.com
+data="$filename-$filesize-registry.${region}.aliyuncs.com/${namespace}/${image}"
+curl -skLo /dev/null "http://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
