a2aa625e629ddfb6faa4aa9990b819d871779ef9
iluckyday <luckyday1990@mail.com>
2021-08-12 14:16:22 +0800

    test

--- get-images.sh
+++ get-images.sh
@@ -12,17 +12,20 @@ chmod +x /tmp/kubeadm
 /tmp/kubeadm config images pull
 docker image list
 
-docker save $(docker image list -q) | xz > /tmp/kubernetes-images-${RELEASE}.tar.xz
+docker save $(docker image list "k8s.gcr.io/*" -q) | xz > /tmp/kubernetes-images-${RELEASE}.tar.xz
 
 # xz -d -k < kubernetes-images-${RELEASE}.tar.xz | docker load
+
+curl -sSkL --remote-name-all https://dl.k8s.io/${RELEASE}/kubernetes-{server,client,node}-linux-amd64.tar.gz
 
 ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
-FILE=/tmp/kubernetes-images-${RELEASE}.tar.xz
-t_data=$(/tmp/transfer wet --silent $FILE)
-
-FILENAME=$(basename $FILE)
-SIZE="$(du -h $FILE | awk '{print $1}')"
-data="$FILENAME-$SIZE-${t_data}"
+for f in /tmp/kubernetes-*z; do
+FILENAME=$(basename $f)
+SIZE=$(du -h $f | awk '{print $1}')
+trans_url=$(/tmp/transfer wet --silent $f)
+[[ -z "$trans_url" ]] && exit
+data="$FILENAME-$SIZE-${trans_url}"
 curl -skLo /dev/null "https://wxpusher.zjiecode.com/api/send/message/?appToken=${WXPUSHER_APPTOKEN}&uid=${WXPUSHER_UID}&content=${data}"
+done
