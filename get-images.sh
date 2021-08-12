58636d7f4d7840b6ebbed64721baf8876f89c317
iluckyday <luckyday1990@mail.com>
2021-08-12 21:10:15 +0800

    test

--- get-images.sh
+++ get-images.sh
@@ -12,11 +12,11 @@ chmod +x /tmp/kubeadm
 /tmp/kubeadm config images pull
 docker image list
 
-docker save $(docker image list "k8s.gcr.io/*" -q) | xz > /tmp/kubernetes-images-${RELEASE}.tar.xz
+docker save $(docker image list "k8s.gcr.io/*" -q) | xz > /tmp/kubernetes-images-${RELEASE}-linux-amd64.tar.xz
 
 # xz -d -k < kubernetes-images-${RELEASE}.tar.xz | docker load
 
-curl -sSkL "https://dl.k8s.io/${RELEASE}/kubernetes-{server,client,node}-linux-amd64.tar.gz" -o "/tmp/kubernetes-#1-linux-amd64.tar.gz"
+curl -sSkL "https://dl.k8s.io/${RELEASE}/kubernetes-{server,client,node}-linux-amd64.tar.gz" -o "/tmp/kubernetes-#1-${RELEASE}-linux-amd64.tar.gz"
 
 ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
