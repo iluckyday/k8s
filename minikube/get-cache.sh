0e5fcf55e99eb7e4a7ec16d8efba8143ec6ce380
iluckyday <luckyday1990@mail.com>
2022-03-01 09:07:47 +0800

    test

--- minikube/get-cache.sh
+++ minikube/get-cache.sh
@@ -6,9 +6,9 @@ sudo apt install -y libvirt-clients
 
 curl -skLO  https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64.tar.gz
 tar -xf  minikube-linux-amd64.tar.gz
-MINIKUBE_VERSION=$(~/out/minikube-linux-amd64 version | awk '/version/ {print $3}')
+MINIKUBE_VERSION=$(./out/minikube-linux-amd64 version | awk '/version/ {print $3}')
 
-~/out/minikube-linux-amd64 start --download-only --driver=kvm2
+./out/minikube-linux-amd64 start --download-only --driver=kvm2
 
 cp minikube-linux-amd64.tar.gz ~/.minikube/
 tar -cJf minikube-"$MINIKUBE_VERSION".tar.xz -C ~/.minikube cache bin minikube-linux-amd64.tar.gz
@@ -16,7 +16,7 @@ tar -cJf minikube-"$MINIKUBE_VERSION".tar.xz -C ~/.minikube cache bin minikube-l
 ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
-for f in ~/minikube-*.tar.xz; do
+for f in ./minikube-*.tar.xz; do
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 trans_url=$(/tmp/transfer wet --silent $f)
